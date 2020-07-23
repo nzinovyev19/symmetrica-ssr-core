1237a1eecb37f40d2169018dd3e66d86413b7a62
Nikita Zinovev <nrz2000@mail.ru>
2020-07-23 18:35:48 +0300

    Create home page

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <main class="main">
+    <section class="home">
+      <div class="container">
+        <div>
+          <h1 class="title">
+            symmetrica-ssr-core
+          </h1>
+          <h3 class="subtitle">{{ title }}</h3>
+          <p>Перейти на страницу: <nuxt-link :to="{ name: 'katalog' }">Каталог</nuxt-link></p>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Главная'
+    };
+  },
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        { hid: 'Home', name: 'Home', content: 'It is home page' }
+      ]
+    };
+  },
+}
+</script>
